38d20999e8754aef8f348a0cc11488b837896f8b
martymods <50667313+martymods@users.noreply.github.com>
2024-12-11 11:26:39 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-ocument.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
     const isSinglePlayer = urlParams.has('singlePlayer');
 
