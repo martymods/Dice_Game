fb338cbdd42153be1a610169bd956a8531d8e8b2
martymods <50667313+martymods@users.noreply.github.com>
2024-12-27 13:38:03 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -5,6 +5,8 @@ import { playerStats, loadStats, saveStats, updateWinStreak, resetWinStreak } fr
 import { addHustler, applyHustlerEffects, updateHustlerUI } from './modules/hustlers.js';
 import { updateUI, showItemPopup, getItemColor } from './modules/ui.js';
 import { itemsList } from './items.js';
+import { playSound } from './modules/audio.js';
+
 
 
 // Fire Status
@@ -600,23 +602,6 @@ if (skipIntroButton) {
                 callback();
             }
         }, 100);
-    }
-
-
-
-    function playSound(sounds, randomize = false) {
-        const soundFile = Array.isArray(sounds) && randomize
-            ? sounds[Math.floor(Math.random() * sounds.length)]
-            : sounds;
-
-        const audio = new Audio(soundFile);
-
-        // Resume audio context if necessary
-        if (typeof audio.resume === "function") {
-            audio.resume().catch(err => console.error("Audio context resume error:", err));
-        }
-
-        audio.play().catch(err => console.error('Audio play error:', err));
     }
 
     function updateUIAfterRoll() {
