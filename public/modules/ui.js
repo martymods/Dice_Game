eb7ff8a356828079fd64856d43bca1758b128be7
martymods <50667313+martymods@users.noreply.github.com>
2024-12-31 18:46:15 -0500

    Update ui.js

--- public/modules/ui.js
+++ public/modules/ui.js
@@ -1,6 +1,6 @@
 import { playSound } from './audio.js';
 import { itemsList } from '/items.js'; // Ensure the correct relative path
-import { updateCollectionDisplay } from './app.js';
+import { updateCollectionDisplay } from '/app.js';
 
 
 
