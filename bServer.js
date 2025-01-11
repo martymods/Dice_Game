d931065a4e459223664ce566071395c6dbc7270b
martymods <50667313+martymods@users.noreply.github.com>
2025-01-11 16:23:48 -0500

    Update bServer.js
    build error

--- bServer.js
+++ bServer.js
@@ -237,8 +237,3 @@ app.use((req, res, next) => {
     next();
 });
 
-const express = require('express');
-app.use(express.static('public'));
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
-});
