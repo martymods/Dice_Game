b7d37271b983a48cba9f99c6a16d6ae7f4a2f218
martymods <50667313+martymods@users.noreply.github.com>
2025-02-25 10:19:36 -0500

    Update squid-game.js

--- public/SG/squid-game.js
+++ public/SG/squid-game.js
@@ -12,7 +12,7 @@ const backgroundTracks = [
 ];
 
 let currentTrack = new Audio(); // Holds the current playing track
-
+const dollMusic = currentTrack; // Redirects any old references to the new system
 const buzzerSound = new Audio("/SG/Buzzer.mp3");
 const countdownSound = new Audio("/SG/CountDown.mp3");
 const countdownEndSound = new Audio("/SG/CountDown_END.mp3");
